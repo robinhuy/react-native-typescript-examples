5cb6c3bc2e14452a22485964ff7c82d66b97aa32
Robin Huy <robin@techmaster.vn>
2021-01-20 12:21:46 +0700

    update dependencies

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import BookingCar from './examples/2-booking-car/App';
 import GmailClone from './examples/3-gmail-clone/app/App';
 
 const App: React.FunctionComponent = () => {
-  // return <Quiz />;
+  return <Quiz />;
   // return <BookingCar />;
-  return <GmailClone />;
+  // return <GmailClone />;
 };
 
 export default App;
